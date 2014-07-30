0ec935cd8cf2c76e2419752e3367c2a7fcf45564
Tu <neil.tu@P21235.local>
2014-07-30 10:00:49 +0900

    modify reverse input

--- reverse/src/com/neil/Source.java
+++ reverse/src/com/neil/Source.java
@@ -1,28 +1,34 @@
 package com.neil;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Source {
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
 
-		// StringBuilder has reverse class
-		// System.out.println(new StringBuilder(in.nextLine()).reverse().toString());
+		// StringBuilder also uses the following method to imploment reverse class.
+		BufferedReader stdReader = new BufferedReader(new InputStreamReader(System.in));
 
-		// StringBuilder uses the following method to imploment reverse class.
-		String input = in.nextLine();
-		char value[] = input.toCharArray();
-		int n = input.length() - 1;
-		for (int j = (n - 1) >> 1; j >= 0; j--) {
-			int k = n - j;
-			char cj = value[j];
-			char ck = value[k];
-			value[j] = ck;
-			value[k] = cj;
+		try {
+			String input = stdReader.readLine();
 
+			char value[] = input.toCharArray();
+			int n = input.length() - 1;
+			for (int j = 0; j <= (n - 1) >> 1; j++) {
+				int k = n - j;
+				char cj = value[j];
+				char ck = value[k];
+				value[j] = ck;
+				value[k] = cj;
+
+			}
+
+			stdReader.close();
+			System.out.println(String.valueOf(value));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-
-		in.close();
-		System.out.print(String.valueOf(value));
 	}
 }
